2cacf3891613f10622580bcb50d972e4ee417d6c
SoulVuazy <milton.erlandsson@utbtrosa.se>
2026-04-28 18:14:15 +0200

    Update DoubleAnchor.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/DoubleAnchor.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/DoubleAnchor.java
@@ -15,8 +15,8 @@ import org.lwjgl.glfw.GLFW;
 
 public final class DoubleAnchor extends Module implements TickListener {
 	public DoubleAnchor() {
-		super(EncryptedString.of("Double Anchor"),
-				EncryptedString.of("Helps you do the air place/double anchor"),
+		super(EncryptedString.of("DbleAnchr"),
+				EncryptedString.of("AchorCheats"),
 				-1,
 				Category.COMBAT);
 	}
