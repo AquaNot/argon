9e8b5947b95f2890d8a65ddf02d7f6b1fba5bf52
SoulVuazy <milton.erlandsson@utbtrosa.se>
2026-04-28 18:22:12 +0200

    Update NoBreakDelay.java

--- src/main/java/dev/lvstrng/argon/module/modules/misc/NoBreakDelay.java
+++ src/main/java/dev/lvstrng/argon/module/modules/misc/NoBreakDelay.java
@@ -4,7 +4,7 @@ import dev.lvstrng.argon.module.Category;
 import dev.lvstrng.argon.module.Module;
 import dev.lvstrng.argon.utils.EncryptedString;
 
-public final class NoBreakDelay extends Module {
+public final class NoBrakDlay extends Module {
 	public NoBreakDelay() {
 		super(EncryptedString.of("No Break Delay"),
 				EncryptedString.of("Removes the break delay from mining blocks"),
