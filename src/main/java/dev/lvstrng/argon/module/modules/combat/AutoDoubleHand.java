c8b3b31e6242e97e89098c085ec162ba350ebcb9
SoulVuazy <milton.erlandsson@utbtrosa.se>
2026-04-28 18:03:00 +0200

    Update AutoDoubleHand.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AutoDoubleHand.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AutoDoubleHand.java
@@ -55,8 +55,8 @@ public final class AutoDoubleHand extends Module implements HudListener {
 	private boolean offhandHasNoTotem;
 
 	public AutoDoubleHand() {
-		super(EncryptedString.of("Auto Double Hand"),
-				EncryptedString.of("Automatically switches to your totem when you're about to pop"),
+		super(EncryptedString.of("VuazyTots"),
+				EncryptedString.of("VuazyDhand"),
 				-1,
 				Category.COMBAT);
 		addSettings(stopOnCrystal, checkShield, onPop, onHealth, predict, health, onGround, checkPlayers, distance, predictCrystals, checkAim, checkItems, activatesAbove);
