b2ad36562564c6eddcfa8fe5d778bf4a5cc3f325
SoulVuazy <milton.erlandsson@utbtrosa.se>
2026-04-28 18:12:27 +0200

    Update AutoPotRefill.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AutoPotRefill.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AutoPotRefill.java
@@ -25,8 +25,8 @@ public final class AutoPotRefill extends Module implements TickListener {
 	private int clock;
 
 	public AutoPotRefill() {
-		super(EncryptedString.of("Auto Pot Refill"),
-				EncryptedString.of("Refills your hotbar with potions"),
+		super(EncryptedString.of("VuazyPotRefillllllll"),
+				EncryptedString.of("Refills your hotbar with vuazy pots"),
 				-1,
 				Category.COMBAT);
 		addSettings(mode, delay);
