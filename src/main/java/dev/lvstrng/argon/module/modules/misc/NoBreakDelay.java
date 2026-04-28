6093e2cdf9c04e6b231b24b28325c85185cbb41e
SoulVuazy <milton.erlandsson@utbtrosa.se>
2026-04-28 18:24:50 +0200

    Fix class name from 'NoBrakDlay' to 'NoBreakDelay'

--- src/main/java/dev/lvstrng/argon/module/modules/misc/NoBreakDelay.java
+++ src/main/java/dev/lvstrng/argon/module/modules/misc/NoBreakDelay.java
@@ -4,7 +4,7 @@ import dev.lvstrng.argon.module.Category;
 import dev.lvstrng.argon.module.Module;
 import dev.lvstrng.argon.utils.EncryptedString;
 
-public final class NoBrakDlay extends Module {
+public final class No Break Delay extends Module {
 	public No Break Delay() {
 		super(EncryptedString.of("NoBreakDlay"),
 				EncryptedString.of("Removes the break delay from mining blocks"),
